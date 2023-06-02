31f121ef8a531b4ae2f50854a1150be81d5546f6
gvpraveen35 <131241370+gvpraveen35@users.noreply.github.com>
2023-06-02 17:55:38 +0530

    Add files via upload

---
+++ Pro2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Pro2
+{
+public static void main(String args[])
+{
+char ch;
+System.out.println("entre the ASCII values");
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+int x=(int)ch;
+System.out.println("ASCII value of"+ch+"is"+x);
+}
+}
+
+
